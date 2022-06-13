ebd22a759ff995f20ad7ff97e8b3bc2aa018520a
Bastien Bouchoux <77078751+Bilibulle@users.noreply.github.com>
2022-06-14 00:01:46 +0200

    update impl and cargo but need pattern refactor

--- src/decryptor.rs
+++ src/decryptor.rs
@@ -1,13 +1,34 @@
-use crate::payment_method_token::PaymentsKeySets;
+use crate::payment_method_token::{GooglePublicKeysEndpoints, PaymentsKeySets};
+
 
 pub trait PublicKeysManager {
-    fn refresh_trusted_signing_key_json(&self) -> PaymentsKeySets;
+    fn new(production: bool) -> Self;
+    fn refresh_trusted_signing_key_json(google_trusted_cert_url: &str) -> PaymentsKeySets;
     fn get_trusted_signing_key_json(&self) -> PaymentsKeySets;
 }
 
 impl PublicKeysManager for PaymentsKeySets {
-    fn refresh_trusted_signing_key_json(&self) -> PaymentsKeySets {
-        todo!()
+
+    fn new(production: bool) -> Self {
+        let endpoints = GooglePublicKeysEndpoints::default();
+        let url: String;
+
+        if production{
+            url = endpoints.keys_url_production;
+        }
+        else {
+            url = endpoints.keys_url_test;
+        }
+        let test = Self::refresh_trusted_signing_key_json(&url);
+
+        Self {
+            keys: test.keys
+        }
+    }
+
+    fn refresh_trusted_signing_key_json(google_trusted_cert_url: &str) -> PaymentsKeySets {
+        let key_sets: PaymentsKeySets = reqwest::get(google_trusted_cert_url)?.json()?;
+        return key_sets;
     }
 
     fn get_trusted_signing_key_json(&self) -> PaymentsKeySets {
--- src/payment_method_token.rs
+++ src/payment_method_token.rs
@@ -1,33 +1,37 @@
 use derivative::Derivative;
+use serde::Deserialize;
 
 pub struct IntermediateSigningKey {
-    signed_key: str,
-    signatures: [str],
+    pub signed_key: String,
+    pub signatures: [String; 3],
 }
 
 pub struct PaymentMethodToken {
-    protocol_version: str,
-    signature: str,
-    intermediate_signing_key: IntermediateSigningKey,
-    signed_message: str,
+    pub protocol_version: String,
+    pub signature: String,
+    pub intermediate_signing_key: IntermediateSigningKey,
+    pub signed_message: String,
 }
 
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct KeySet {
-    key_value: str,
-    protocol_version: str,
-    key_expiration: str,
+    pub key_value: String,
+    pub protocol_version: String,
+    pub key_expiration: String,
 }
 
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct PaymentsKeySets {
-    keys: [KeySet; 3]
+    pub keys: [KeySet; 3],
 }
 
 #[derive(Derivative)]
-#[derivative(Debug, Default)]
+#[derivative(Default)]
 pub struct GooglePublicKeysEndpoints {
-    #[derivative(Default(value = "https://payments.developers.google.com/paymentmethodtoken/keys.json"))]
-    keys_url_production: str,
-    #[derivative(Default(value = "https://payments.developers.google.com/paymentmethodtoken/test/keys.json"))]
-    keys_url_test: str,
-
+    #[derivative(Default(value="https://payments.developers.google.com/paymentmethodtoken/keys.json"))]
+    pub keys_url_production: String,
+    #[derivative(Default(value="https://payments.developers.google.com/paymentmethodtoken/test/keys.json"))]
+    pub keys_url_test: String,
 }
